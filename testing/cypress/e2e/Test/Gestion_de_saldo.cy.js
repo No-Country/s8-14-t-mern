b5eb5c32de07fd68093d9f31587cd29487fc53e2
Maxi.Barbo <maxi10lb@gmail.com>
2023-05-10 20:58:54 -0300

    Test1

---
+++ testing/cypress/e2e/Test/Gestion_de_saldo.cy.js
@@ -0,0 +1,11 @@
+describe('Seccion Registro de Usuarios', () => {
+
+    it('Gestion_001 | Realizar una transferencia', () => {
+        
+    });
+
+    it('Gestion_002 | Agregar Trajeta Virtual', () => {
+        
+    });
+    
+});
---
+++ testing/cypress/e2e/Test/Registro_usuario.cy.js
@@ -0,0 +1,7 @@
+describe('Seccion Registro de Usuarios', () => {
+
+    it('Registro_001 | Registro exitoso de usuario', () => {
+        
+    });
+    
+});
