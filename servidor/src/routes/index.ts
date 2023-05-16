579f373c8b9d7a7524f32a1822d0ea2cd4dc7284
Pablo Swis <94465269+Pabloswis@users.noreply.github.com>
2023-05-16 18:37:02 -0300

    create model user

--- servidor/src/routes/index.ts
+++ servidor/src/routes/index.ts
@@ -4,36 +4,34 @@ const mainRouter = Router()
 const PATH_ROUTES = `${__dirname}`
 
 type ModuleRouter = {
-	router: Router;
+  router: Router
 }
 
 // Remover extension del nombre del archivo
 const removeExtension = (filename: string): string => {
-	return filename.split('.').shift() as string
+  return filename.split('.').shift() as string
 }
-
 // Import de todos los routers del directorio /routes
 readdirSync(PATH_ROUTES)
-	.filter(file => {
-		const cleanName = removeExtension(file)
-		return cleanName !== 'index'
-	})
-	.forEach(file => {
-		const cleanName = removeExtension(file)
-		import(`./${cleanName}.routes`)
-			.then((moduleRouter: ModuleRouter) => {
-				console.log(`Cargando ruta: ${cleanName} ...`)
-				mainRouter.use(`/${cleanName}`, moduleRouter.router)
-			})
-			.catch(err => {
-				console.log(`No se pudo cargar ruta ${cleanName}`, err)
-			})
-
-	})
+  .filter(file => {
+    const cleanName = removeExtension(file)
+    return cleanName !== 'index'
+  })
+  .forEach(file => {
+    const cleanName = removeExtension(file)
+    import(`./${cleanName}.routes`)
+      .then((moduleRouter: ModuleRouter) => {
+        console.log(`Cargando ruta: ${cleanName} ...`)
+        mainRouter.use(`/${cleanName}`, moduleRouter.router)
+      })
+      .catch(err => {
+        console.log(`No se pudo cargar ruta ${cleanName}`, err)
+      })
+  })
 
 //default index
 mainRouter.get('/', (_req: Request, res: Response) => {
-	res.send('Ok')
+  res.send('Ok')
 })
 
-export default mainRouter 
+export default mainRouter
