21f50febeb682f882593f8d73e15077079191d2b
Gasnis <113210850+Gasnis@users.noreply.github.com>
2023-05-17 20:10:31 +0200

    errores todavia

--- servidor/src/services/users.services.ts
+++ servidor/src/services/users.services.ts
@@ -14,10 +14,43 @@ const fetchGet = async() => {
 
 const fetchPut = async(user: any) => {
   try {
-    
-    
+    const userMatch = await User.find({ id: user.id });
+    firstName
+lastname
+typeIdentification
+numberIdentification
+alias
+phoneNumber
+email
+address
+avatar
+password
+balance
+isActive
+rol
+token
+    const firstName = user.firstName !== "" ? user.firstName : userMatch.firstName;
+    const lastname = user.lastname !== "" ? user.lastname : userMatch.lastname;
+    const typeIdentification = user.typeIdentification !== "" ? user.typeIdentification : userMatch.typeIdentification
+    const price = user.price !== "" ? user.price : userMatch.price
+    const time = user.time !== "" ? user.time : userMatch.time
+    const description = user.description !== "" ? user.description : userMatch.description
+    const available = user.available !== "" ? user.available : userMatch.available
+
+    const resp = await User.findByIdAndUpdate(
+      user.id,
+      {
+        $set: { firstName, image, category, price, time, description, available },
+      },
+      { new: true }
+    );
+    return {
+      error: false,
+      data: resp,
+    };
   } catch (error) {
-    
+    console.log(error);
+    return { data: error };
   }
 }
 
