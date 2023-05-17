c43f2c64bba53961164d20cf3fe684a280f84ba7
ismaelnelro <ismaelnelsonrosas@hotmail.com>
2023-05-17 17:01:40 -0300

    rama beneficios

--- cliente/src/pages/HomePage.tsx
+++ cliente/src/pages/HomePage.tsx
@@ -1,9 +1,12 @@
-import { Card } from "@tremor/react";
 import { Outlet } from "react-router-dom";
+import Benefits from "@/components/BenefitHomeComponent";
+import PanelHome from "@/components/PanelHomeComponent";
+import { Button, Card } from "@tremor/react";
 export default function HomePage(): JSX.Element {
   return (
     <div>
-      HomePage<Card>card</Card>
+      <Benefits />
+      <PanelHome />
       <Outlet />
     </div>
   );
