f843224d5f7eb24e580f8eb966118723cdc4b409
Pablo Swis <94465269+Pabloswis@users.noreply.github.com>
2023-05-17 17:50:06 -0300

    agregue ruta login y hash password al create

--- servidor/src/controller/users.controllers.ts
+++ servidor/src/controller/users.controllers.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express'
-import { fetchGet, fetchPost } from '../services/users.services'
+import { fetchGet, fetchLogin, fetchPost } from '../services/users.services'
 
 const getUserCtrl = (_req: Request, res: Response) => {
   fetchGet()
@@ -16,10 +16,19 @@ const postUserCtrl = async (req: Request, res: Response) => {
   try {
     const data = await fetchPost(req.body)
     res.status(201).json({ msg: 'User created succeful', data })
-  } catch (error: any) {
-    console.log('CONTROLADOR', error)
-    res.status(400).json({ error: error.message })
+  } catch (error) {
+    if (error instanceof Error) res.status(400).json({ error: error.message })
   }
 }
 
-export { getUserCtrl, postUserCtrl }
+const loginUser = async (req: Request, res: Response) => {
+  try {
+    const { password, email } = req.body
+    const data = await fetchLogin(password, email)
+    res.status(201).json({ msg: 'User login succeful', data })
+  } catch (error) {
+    if (error instanceof Error) res.status(400).json({ error: error })
+  }
+}
+
+export { getUserCtrl, postUserCtrl, loginUser }
