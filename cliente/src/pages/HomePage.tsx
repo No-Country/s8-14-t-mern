6eccf99f7aa153697fc3012082e5c07523c2b1a9
ismaelnelro <ismaelnelsonrosas@hotmail.com>
2023-05-23 09:21:06 -0300

    [feat]: ResponsePage and Buttonc Components

--- cliente/src/pages/HomePage.tsx
+++ cliente/src/pages/HomePage.tsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Benefits from "@/components/BenefitHomeComponent";
 import PanelHome from "@/components/PanelHomeComponent";
-import { Button, Card } from "@tremor/react";
 import Header from "../components/Header";
 import Lists from "../components/Lists";
 
