0fc1ce7d240d826d67a2a73338538774a4cdcdf5
Maxi.Barbo <maxi10lb@gmail.com>
2023-05-25 20:07:20 -0300

    test-gestion-saldo

--- testing/cypress/e2e/Test/HtmlProperties.cy.js
+++ testing/cypress/e2e/Test/HtmlProperties.cy.js
@@ -2,17 +2,22 @@ import { loginPage } from "../../pages/loginPage";
 import { localStorage } from "../../pages/localStorage";
 import { fetchApis } from "../../pages/fetchApi";
 
+const LocalHost = 'http://localhost:3000'
 const LocalHostUrl_login = 'http://localhost:3000/auth/login'
 const localApi = 'http://localhost:9000/api/v1/pigmeo/users'
+const LocalHostUrl = 'http://localhost:3000/home'
 
-describe('Html Head Properties', () => {
+describe('Html Properties', () => {
     beforeEach(() => {
        cy.visit(LocalHostUrl_login)
        loginPage.userLogin('user@mail.com','Abcd1234*')
     });
 
-    it.only('Proiedades en Head del Html', () => {
-      
+    it.only('Html Request', () => {
+      cy.request(LocalHost)
+        .then((response)=>{
+            expect(response).property('status').to.eq(200)
+        })
     });
 
     it('Validacion elementos en LocalStorage / Cookies', () => {
