4146a360095515d888fff01cdb778fb5538a5c46
ismaelnelro <ismaelnelsonrosas@hotmail.com>
2023-06-02 18:02:12 -0300

    cambios en benefit

--- cliente/src/pages/BenefitPage.tsx
+++ cliente/src/pages/BenefitPage.tsx
@@ -1,61 +1,78 @@
 import HeaderBackButton from '@/components/HeaderBackButton'
 import { Card, Col, Flex, Grid, Icon, Subtitle, TextInput, Title, Text } from '@tremor/react'
-import { FilmIcon, SearchIcon, ShoppingBagIcon, ShoppingCartIcon } from '@heroicons/react/outline'
 import { ComponentType, SVGProps } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
+import { Link } from 'react-router-dom';
+import { FilmIcon, SearchIcon, ShoppingBagIcon, ShoppingCartIcon, GlobeIcon } from '@heroicons/react/outline'
 
 interface ListItemType {
-  title: string;
+  href: string;
   ListIcon: ComponentType<SVGProps<SVGSVGElement>>;
 }
 
 
 const CATEGORY_ITEMS: ListItemType[] = [
   {
-    title: "Entrenamiento",
+    href: "/addFunds",
     ListIcon: FilmIcon
   },
   {
-    title: "Compras",
+    href: "/addFunds",
     ListIcon: ShoppingBagIcon
   },
   {
-    title: "Supermercado",
+    href: "/addFunds",
     ListIcon: ShoppingCartIcon
+  },
+  {
+    href: "/addFunds",
+    ListIcon: GlobeIcon
   }
 ]
 
 
-const benefit = [
-  {
-    company: "Coto",
-    benefit: "8% descount"
-  },
-  {
-    company: "Mc Donal",
-    benefit: "28% descount"
-  },
-  {
-    company: "Burger King",
-    benefit: "18% descount"
-  },
-  {
-    company: "Vea Supermarket",
-    benefit: "18% descount"
-  },
-  {
-    company: "Vea Supermarket",
-    benefit: "18% descount"
-  }
-]
+
+interface ActionItem {
+  title: string;
+  href: string;
+  ListIcon: ComponentType<SVGProps<SVGSVGElement>>;
+}
+
+// const BENEFICE_ITEMS: ActionItem[] = [
+//   {
+//     title: "Coto",
+//     href: "/addFunds",
+//     icon: 
+//   },
+//   {
+//     title: "Mc Donal",
+//     href: "/addFunds",
+//     icon: "28% descount"
+//   },
+//   {
+//     title: "Burger King",
+//     href: "/addFunds",
+//     icon: "18% descount"
+//   },
+//   {
+//     title: "Vea Supermarket",
+//     href: "/addFunds",
+//     icon: "18% descount"
+//   },
+//   {
+//     title: "Vea Supermarket",
+//     href: "/addFunds",
+//     icon: "18% descount"
+//   }
+// ]
 
 
 
 export default function BenefitPage() {
   return (
-    <div className='p-1 w-96'>
-      <HeaderBackButton title="Beneficios"/>
+    <div>
+      <HeaderBackButton title="Beneficios" />
       <Grid numColsSm={12} className='mt-8'>
         <Col numColSpan={12} className='flex justify-center'>
           <TextInput icon={SearchIcon} placeholder='Busco un veneficio' className='bg-slate-300 p-2' />
@@ -64,20 +81,22 @@ export default function BenefitPage() {
         <Col numColSpan={11} className='mt-10'>
           <Title className='mb-6'> Selecinar la categoria</Title>
           <Flex className='justify-around my-4 '>
+
             {CATEGORY_ITEMS.map((item, index) => (
               <div className='flex flex-col items-center'>
-                <Card key={index} className='w-16 h-16 flex justify-center bg-slate-600'>
-                  <Icon icon={item.ListIcon} />
+                <Card key={index} className='w-16 h-16 flex justify-center bg-secondary'>
+                  <Icon icon={item.ListIcon} className='text-[#3B1B80]' />
                 </Card>
                 <Subtitle className='w-full text-center'>
                   {item.title}
                 </Subtitle>
               </div>
             ))}
+
           </Flex>
         </Col>
         <Col numColSpan={12} className='mt-10'>
-          <Title  className='mb-6'> Lo mas frecuente</Title>
+          <Title className='mb-6'> Lo mas frecuente</Title>
           <div>
 
             <Swiper
@@ -88,7 +107,7 @@ export default function BenefitPage() {
               onSwiper={(swiper) => console.log(swiper)}
             >
 
-              {benefit.map((card) => (
+              {/* {benefit.map((card) => (
                 <SwiperSlide>
                   <Card
                     className="w-64 h-auto mt-2"
@@ -99,7 +118,7 @@ export default function BenefitPage() {
                     <Text>{card.benefit}</Text>
                   </Card>
                 </SwiperSlide>
-              ))}
+              ))} */}
             </Swiper>
           </div>
         </Col>
