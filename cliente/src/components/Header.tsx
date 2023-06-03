998945b69f76b81e8c64382e9af529e703e2d0be
Juan Antonio Ayola Cortes <antonio.ayola.cortes@gmail.com>
2023-06-03 10:38:59 -0600

    feat(front): home page show balance & hide balance button

--- cliente/src/components/Header.tsx
+++ cliente/src/components/Header.tsx
@@ -1,16 +1,20 @@
+import { useState } from "react";
+
 import { Card, Metric, Text, Icon } from "@tremor/react";
 import { BellIcon, EyeIcon } from "@heroicons/react/outline";
 import { useUserData } from "@/context/UserContext";
 
 export default function Header(): JSX.Element {
   const { user } = useUserData();
+  const [showBalance, setShowBalance] = useState(false);
+
+  const handleClickShowBalance = () => setShowBalance((show) => !show);
 
   return (
-
-    <Card className="h-[11.75rem] bg-gradient-to-r from-[#661EC2] from-0% to-[#240860] to-100% ">
+    <Card className="h-[11.75rem] bg-gradient-to-r from-[#661EC2] from-0% to-[#240860] to-100% rounded-none">
       <div className="flex  items-center">
-        <div className="w-9 h-8 bg-white rounded-md text-center text-[#1205A4] mt-3">
-        {user?.firstName && user.firstName.slice(0,2)}
+        <div className="w-9 h-8 bg-white rounded-md text-center text-[#1205A4] mt-3 grid place-items-center uppercase">
+          {user?.firstName && user.firstName.slice(0, 2)}
         </div>
         <Icon
           className="absolute top-4 right-0 h-16 w-16 text-white"
@@ -21,8 +25,14 @@ export default function Header(): JSX.Element {
       <div className="flex flex-col items-center">
         <Text className="text-white">Total disponible</Text>
         <div className="flex items-center">
-          <Metric className="text-white">$ 120.000</Metric>
-          <Icon className="text-white" size="md" icon={EyeIcon} />
+          <Metric className="text-white">
+            {showBalance
+              ? `$ ${user?.balance?.toLocaleString()}`
+              : "**********"}
+          </Metric>
+          <button onClick={handleClickShowBalance}>
+            <Icon className="text-white" size="md" icon={EyeIcon} />
+          </button>
         </div>
       </div>
     </Card>
