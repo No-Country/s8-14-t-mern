6208a3bbcadf6f4cb8c42890c714f8ee2ab0ebb5
Lautaro Durán <84945392+LautaroLD@users.noreply.github.com>
2023-06-04 15:52:18 -0300

    Fix/add details transactions (#103)
    * add styles & details to transactions
    
    * fix detail

--- cliente/src/pages/TransactionsPage.tsx
+++ cliente/src/pages/TransactionsPage.tsx
@@ -8,13 +8,14 @@ import { TransactionItem } from "@/components/TransactionItemComponent";
 
 export default function TransactionsPage() {
   const [transactions, setTransactions] = useState<ITransactions[] | []>([]);
+  const [loading, setLoading] = useState(true)
   const { user } = useUserData();
   useEffect(() => {
     (async () => {
       try {
         const res = await getUserTransactions(user?.id);
-        console.log(res);
         setTransactions(res.data.data);
+        setLoading(false)
       } catch (error) {
         console.log(error);
       }
@@ -27,19 +28,23 @@ export default function TransactionsPage() {
 
       <section className="bg-primary text-white flex flex-col items-center h-[170px] gap-4">
         <p className=" text-[1.13rem]">Saldo Pigmeo disponible</p>
-        <p className=" font-bold text-[1.5rem]">
-          $ {user?.balance?.toLocaleString()}
-        </p>
+        <p className=" font-bold text-[1.5rem]">${user.balance}</p>
       </section>
       <section className="relative h-fit">
+
         <List className=" bg-white border rounded-md w-[90%] left-[5%] relative top-[-70px]">
-          {transactions.length ? (
-            transactions.map((item) => TransactionItem({ item, inHome: false }))
-          ) : (
+          {loading ?
             <div className="flex h-[50px] ">
-              <p className="m-auto">No tienes ningún movimiento</p>
-            </div>
-          )}
+              <p className="m-auto">Cargando...</p>
+            </div> :
+            transactions.length ? (
+              transactions.map((item, index) => TransactionItem({ item, inHome: false, index, place:"transactionPage" }))
+            ) : (
+              <div className="flex h-[50px] ">
+                <p className="m-auto">No tienes ningún movimiento</p>
+              </div>
+            )
+          }
         </List>
       </section>
     </>
